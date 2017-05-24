9f64312bf92e3fd8956786f9a96d9563739a3ea8
Dmitriy Nikitin <mnikitin.dmitri@yandex.ru>
2017-05-25 01:57:43 +0300

    Access

--- app/src/main/java/com/example/test/Screen2.java
+++ app/src/main/java/com/example/test/Screen2.java
@@ -5,20 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class Screen2 extends AppCompatActivity{
 
-WebView page;
+    WebView page;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
 
+
+        String name = "https://oauth.vk.com/authorize";
+
         page = (WebView) new WebView(this);
         setContentView(page);
 
-        page.loadUrl("https://oauth.vk.com/authorize?client_id=6043385&display=mobile&redirect_uri=&scope=friends&response_type=code&v=5.64");
+       //page.loadUrl("https://oauth.vk.com/authorize?client_id=6043385&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,offline&response_type=code&v=5.64");
+        page.loadUrl("https://oauth.vk.com/authorize?client_id=6043385&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,offline&response_type=token&v=5.64");
     }
 
 
