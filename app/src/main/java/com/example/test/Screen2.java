f114983b39a9ca228ea352f60ab818fdf5051e88
Dmitriy Nikitin <mnikitin.dmitri@yandex.ru>
2017-05-26 01:11:25 +0300

    Str Tok

--- app/src/main/java/com/example/test/Screen2.java
+++ app/src/main/java/com/example/test/Screen2.java
@@ -1,33 +1,50 @@
 package com.example.test;
 
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+
+
+import static com.example.test.R.id.web;
 
 
 public class Screen2 extends AppCompatActivity{
 
     WebView page;
-    String url;
+    String url, token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
+/*
+        WebView webView = (WebView) findViewById(R.id.web);
+        Uri data = getIntent().getData();
+        webView.loadUrl(data.toString());
+*/
+        page = (WebView) findViewById(web);
+        page.setWebViewClient(new WebViewClient());
+        page.getSettings().getBuiltInZoomControls();
 
-
-        String name = "https://oauth.vk.com/authorize";
-
-        page = (WebView) new WebView(this);
-        setContentView(page);
-
+        url = "https://oauth.vk.com/authorize?client_id=6043385&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,offline&response_type=token&v=5.64&revoke=1";
        //page.loadUrl("https://oauth.vk.com/authorize?client_id=6043385&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,offline&response_type=code&v=5.64");
-        page.loadUrl("https://oauth.vk.com/authorize?client_id=6043385&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,offline&response_type=token&v=5.64");
+        page.loadUrl(url);
+
+
+
+
     }
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        token = page.getUrl();
+        Toast.makeText(this, token, Toast.LENGTH_LONG).show();
+    }
 }
