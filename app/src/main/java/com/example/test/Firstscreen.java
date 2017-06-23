42669cda489d1b58dc4f555c09336604b32efa87
Dmitriy Nikitin <mnikitin.dmitri@yandex.ru>
2017-06-23 15:59:31 +0300

    List

--- app/src/main/java/com/example/test/Firstscreen.java
+++ app/src/main/java/com/example/test/Firstscreen.java
@@ -1,38 +1,23 @@
 package com.example.test;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Firstscreen extends AppCompatActivity implements View.OnClickListener{
+public class Firstscreen extends ListActivity {
 
     Button OnMainScreen;
-    Button on2screen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firstscreen);
 
-        OnMainScreen = (Button) findViewById(R.id.OnMainScreen);
-        on2screen = (Button) findViewById(R.id.on2screen);
-
-        OnMainScreen.setOnClickListener(this);
-        on2screen.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.OnMainScreen:
-                Intent MainScreen = new Intent(this, MainActivity.class);
-                startActivity(MainScreen);
-                break;
-            case R.id.on2screen:
-                Intent Screen2= new Intent(this, Screen2.class);
-                startActivity(Screen2);
-            }
-        }
+
 }
 
--- app/src/main/java/com/example/test/Screen2.java
+++ app/src/main/java/com/example/test/Screen2.java
@@ -1,81 +1,210 @@
 package com.example.test;
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebResourceRequest;
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 
 import static com.example.test.R.id.web;
 
+    public class Screen2 extends AppCompatActivity{
 
-public class Screen2 extends AppCompatActivity{
+        private String TAG = MainActivity.class.getSimpleName();
 
-    final String SAVED_TEXT = "save_text", SAVED_FRIEND ="save_friend";
-
-    WebView page;
-    String url1, token, tok, access_token, user_ID, friend, home, ans ,host;
-    SharedPreferences sPref;
-    StringTokenizer str, access;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_screen2);
-
-        page = (WebView) findViewById(web);
-        page.setWebViewClient(new MyWebViewClient());
+        private ProgressDialog pDialog;
+        private ListView lv;
 
 
-        url1 = "https://oauth.vk.com/authorize?client_id=6043385&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,offline&response_type=token&v=5.65&revoke=1";
-        page.loadUrl(url1);
+        WebView page;
+        String url1, token, tok, access_token;
+        SharedPreferences sPref;
+        StringTokenizer str, access;
 
-
-    }
-
-    public class MyWebViewClient extends WebViewClient {
+        // URL to get contacts JSON
+        private static String JSON = null;
+        ArrayList<HashMap<String, String>> contactList;
 
 
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-           view.loadUrl(url);
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_screen2);
 
-           token = view.getUrl();
-            Uri uri = Uri.parse(token);
-            tok = uri.getFragment();
-            if(tok != null)
-            {
-            access = new StringTokenizer(tok, "&");
-            access_token = access.nextToken();
+            page = (WebView) findViewById(web);
+            page.setWebViewClient(new MyWebViewClient());
 
-                view.loadUrl("https://api.vk.com/method/friends.get?fields=photo_100,country&order=name&" + access_token);
+
+            url1 = "https://oauth.vk.com/authorize?client_id=6043385&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,offline&response_type=token&v=5.65&revoke=1";
+            page.loadUrl(url1);
+        }
+
+        public class MyWebViewClient extends WebViewClient {
+
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+
+                token = view.getUrl();
+                Uri uri = Uri.parse(token);
+                tok = uri.getFragment();
+                if(tok != null)
+                {
+                    access = new StringTokenizer(tok, "&");
+                    access_token = access.nextToken();
+
+                    view.loadUrl("https://api.vk.com/method/friends.get?fields=photo_100,country&order=name&" + access_token);
+                    JSON = "https://api.vk.com/method/friends.get?fields=photo_100,country&order=name&" + access_token;
+                    view.setVisibility(view.GONE);
+                    contactList = new ArrayList<>();
+
+                    lv = (ListView) findViewById(R.id.list);
+                    new GetContacts().execute();
+                }
+
+
+                return true;
+            }
+        }
+
+        /**
+         * Async task class to get json by making HTTP call
+         */
+        private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                // Showing progress dialog
+                pDialog = new ProgressDialog(Screen2.this);
+                pDialog.setMessage("Please wait...");
+                pDialog.setCancelable(false);
+                pDialog.show();
+
             }
 
+            @Override
+            protected Void doInBackground(Void... arg0) {
+                HttpHandler sh = new HttpHandler();
 
-               return true;
+                // Making a request to url and getting response
+                String jsonStr = sh.makeCall(JSON);
+
+                Log.e(TAG, "Response from url: " + jsonStr);
+
+                if (jsonStr != null) {
+                    try {
+                        JSONObject jsonObj = new JSONObject(jsonStr);
+
+                        // Getting JSON Array node
+                        JSONArray contacts = jsonObj.getJSONArray("response");
+
+                        // looping through All Contacts
+                        for (int i = 0; i < contacts.length(); i++) {
+                            JSONObject c = contacts.getJSONObject(i);
+
+                            String id = c.getString("uid");
+                            String first_name = c.getString("first_name");
+                            String last_name = c.getString("last_name");
+
+
+                            // tmp hash map for single contact
+                            HashMap<String, String> contact = new HashMap<>();
+
+                            // adding each child node to HashMap key => value
+                            contact.put("uid", id);
+                            contact.put("first_name", first_name);
+                            contact.put("last_name", last_name);
+
+
+                            // adding contact to contact list
+                            contactList.add(contact);
+                        }
+                    } catch (final JSONException e) {
+                        Log.e(TAG, "Json parsing error: " + e.getMessage());
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(),
+                                        "Json parsing error: " + e.getMessage(),
+                                        Toast.LENGTH_LONG)
+                                        .show();
+                            }
+                        });
+
+                    }
+                } else {
+                    Log.e(TAG, "Couldn't get json from server.");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Couldn't get json from server. Check LogCat for possible errors!",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+                // Dismiss the progress dialog
+                if (pDialog.isShowing())
+                    pDialog.dismiss();
+                /**
+                 * Updating parsed JSON data into ListView
+                 * */
+                ListAdapter adapter = new SimpleAdapter(
+                        Screen2.this, contactList,
+                        R.layout.list_item, new String[]{"first_name",
+                        "last_name"}, new int[]{
+                        R.id.first_name, R.id.last_name});
+
+                lv.setAdapter(adapter);
+            }
+
+        }
+
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            //Toast.makeText(this, token, Toast.LENGTH_LONG).show();
+            //Toast.makeText(this, tok, Toast.LENGTH_LONG).show();
+            //Toast.makeText(this, access_token, Toast.LENGTH_LONG).show();
+            //Toast.makeText(this, ans, Toast.LENGTH_LONG).show();
+
+
         }
     }
-
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //Toast.makeText(this, token, Toast.LENGTH_LONG).show();
-        //Toast.makeText(this, tok, Toast.LENGTH_LONG).show();
-        //Toast.makeText(this, access_token, Toast.LENGTH_LONG).show();
-        //Toast.makeText(this, ans, Toast.LENGTH_LONG).show();
-
-
-    }
-}
